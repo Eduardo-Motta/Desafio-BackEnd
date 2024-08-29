ba8a1db4b730f01d61afeb9d1fc3b1372e0bf589
Elias Lima <eduardomotta.edu@outlook.com>
2024-08-29 00:49:39 -0400

    refactor: update database scripts

--- scripts/create-tables.sql
+++ scripts/create-tables.sql
@@ -1,19 +1,28 @@
-CREATE TABLE Motorcycle (
+CREATE TABLE "User" (
     "Id" UUID PRIMARY KEY,
-    "Year" INTEGER NOT NULL,
-    "Model" VARCHAR(200) NOT NULL,
-    "LicensePlate" VARCHAR(20) NOT NULL,
+    "Identity" VARCHAR(14) NOT NULL,
+    "Password" VARCHAR(255) NOT NULL,
+    "Role" VARCHAR(20) NOT NULL,
     "CreatedAt" TIMESTAMP NOT NULL,
     "UpdatedAt" TIMESTAMP
 );
 
-CREATE UNIQUE INDEX "IX_LicensePlate" ON Motorcycle ("LicensePlate");
+CREATE TABLE "Motorcycle" (
+    "Id" UUID PRIMARY KEY,
+    "Year" INTEGER NOT NULL,
+    "Model" VARCHAR(200) NOT NULL,
+    "LicensePlate" VARCHAR(20) NOT NULL UNIQUE,
+    "CreatedAt" TIMESTAMP NOT NULL,
+    "UpdatedAt" TIMESTAMP
+);
+
+CREATE UNIQUE INDEX "IX_LicensePlate" ON "Motorcycle" ("LicensePlate");
 
 CREATE TABLE "Courier" (
     "Id" UUID PRIMARY KEY,
     "Name" VARCHAR(255) NOT NULL,
     "Cnpj" VARCHAR(14) NOT NULL,
-    "DrivingLicense" VARCHAR(9) NOT NULL,
+    "DrivingLicense" VARCHAR(11) NOT NULL,
     "DrivingLicenseCategory" VARCHAR(2) NOT NULL,
     "DrivingLicensePath" TEXT,
     "BirthDate" DATE NOT NULL,
@@ -28,4 +37,79 @@ CREATE TABLE "Plan" (
     "DailyFineRate" DECIMAL(18, 2) NOT NULL,
     "CreatedAt" TIMESTAMP NOT NULL,
     "UpdatedAt" TIMESTAMP
+);
+
+CREATE TABLE "Rent" (
+    "Id" UUID PRIMARY KEY,
+    "CourierId" UUID NOT NULL,
+    "PlanId" UUID NOT NULL,
+    "MotorcycleId" UUID NOT NULL,
+    "StartDate" TIMESTAMP NOT NULL,
+    "ExpectedEndDate" TIMESTAMP NOT NULL,
+    "EndDate" TIMESTAMP,
+    "Status" VARCHAR(50) NOT NULL,
+    "TotalPayment" DECIMAL(18, 2) NOT NULL,
+    "CreatedAt" TIMESTAMP NOT NULL,
+    "UpdatedAt" TIMESTAMP,
+
+    CONSTRAINT "FK_Courier"
+      FOREIGN KEY("CourierId")
+	  REFERENCES "Courier"("Id"),
+
+    CONSTRAINT "FK_Plan"
+      FOREIGN KEY("PlanId")
+	  REFERENCES "Plan"("Id"),
+
+    CONSTRAINT "FK_Motorcycle"
+      FOREIGN KEY("MotorcycleId")
+	  REFERENCES "Motorcycle"("Id")
+);
+
+CREATE TABLE "RentalClosure" (
+    "Id" UUID PRIMARY KEY,
+    "RentId" UUID NOT NULL,
+    "EndDate" TIMESTAMP,
+    "ExceededDays" INTEGER NOT NULL,
+    "PenaltyAmountForUnusedDay" DECIMAL(18, 2) NOT NULL,
+    "CostForUsedDays" DECIMAL(18, 2) NOT NULL,
+    "TotalAdditionalDailyAmount" DECIMAL(18, 2) NOT NULL,
+    "TotalPayment" DECIMAL(18, 2) NOT NULL,
+    "CreatedAt" TIMESTAMP NOT NULL,
+    "UpdatedAt" TIMESTAMP,
+
+    CONSTRAINT "FK_Rent"
+      FOREIGN KEY("RentId")
+	  REFERENCES "Rent"("Id")
+);
+
+CREATE TABLE "Order" (
+    "Id" UUID PRIMARY KEY,
+    "DeliveryStatus" VARCHAR(20) NOT NULL,
+    "CreatedAt" TIMESTAMP NOT NULL,
+    "UpdatedAt" TIMESTAMP
+);
+
+CREATE TABLE "DeliveryAssignment" (
+    "Id" UUID PRIMARY KEY,
+    "CourierId" UUID NOT NULL,
+    "OrderId" UUID NOT NULL,
+    "AssignedAt" TIMESTAMP NOT NULL,
+    "CompletedAt" TIMESTAMP,
+    "CreatedAt" TIMESTAMP NOT NULL,
+    "UpdatedAt" TIMESTAMP,
+
+    CONSTRAINT "FK_Courier"
+      FOREIGN KEY("CourierId")
+	  REFERENCES "Courier"("Id"),
+
+    CONSTRAINT "FK_Order"
+      FOREIGN KEY("OrderId")
+	  REFERENCES "Order"("Id")
+);
+
+CREATE TABLE "MotorcycleNotification" (
+    "Id" UUID,
+    "Year" INTEGER NOT NULL,
+    "Model" VARCHAR(200) NOT NULL,
+    "LicensePlate" VARCHAR(20) NOT NULL
 );
--- scripts/seeding.sql
+++ scripts/seeding.sql
@@ -1,3 +1,8 @@
+INSERT INTO "User" ("Id", "Identity", "Password", "Role", "CreatedAt", "UpdatedAt")
+VALUES
+    (gen_random_uuid(), '81216414000154', '8iqxgDnB/2tXe5G2o1WTMw==', 'Admin', NOW(), NULL),
+    ('a2284da1-e871-4e57-8af1-fce994aa1824', '12345678000195', '8iqxgDnB/2tXe5G2o1WTMw==', 'Courier', NOW(), NULL);
+
 INSERT INTO "Plan" ("Id", "Days", "DailyRate", "DailyFineRate", "CreatedAt", "UpdatedAt")
 VALUES
     (gen_random_uuid(), 7, 30.0, 20.0, NOW(), NULL),
@@ -8,16 +13,28 @@ VALUES
 
 INSERT INTO "Motorcycle" ("Id", "Year", "Model", "LicensePlate", "CreatedAt", "UpdatedAt")
 VALUES
-    (gen_random_uuid(), 2020, 'Honda CBR600RR', 'ABC1234', NOW(), NULL),
-    (gen_random_uuid(), 2019, 'Yamaha R1', 'XYZ5678', NOW(), NULL),
+    (gen_random_uuid(), 2020, 'Honda CBR600RR', 'OSW9047', NOW(), NULL),
+    (gen_random_uuid(), 2019, 'Yamaha R1', 'CBA8H38', NOW(), NULL),
     (gen_random_uuid(), 2021, 'Kawasaki Ninja ZX-10R', 'LMN9101', NOW(), NULL),
     (gen_random_uuid(), 2022, 'Suzuki GSX-R1000', 'OPQ1122', NOW(), NULL),
-    (gen_random_uuid(), 2023, 'Ducati Panigale V4', 'RST3344', NOW(), NULL);
+    (gen_random_uuid(), 2023, 'Ducati Panigale V4', 'XJC6D45', NOW(), NULL);
 
 INSERT INTO "Courier" ("Id", "Name", "Cnpj", "DrivingLicense", "DrivingLicenseCategory", "DrivingLicensePath", "BirthDate", "CreatedAt", "UpdatedAt")
 VALUES
-    (gen_random_uuid(), 'João Silva', '12345678000195', '123456789', 'A', 'path/to/license1.png', '1985-03-15', NOW(), NULL),
-    (gen_random_uuid(), 'Maria Oliveira', '98765432000185', '987654321', 'B', 'path/to/license2.png', '1990-07-22', NOW(), NULL),
-    (gen_random_uuid(), 'Carlos Souza', '19283746000199', '456789123', 'AB', 'path/to/license3.png', '1982-11-30', NOW(), NULL),
-    (gen_random_uuid(), 'Ana Costa', '56473829000172', '789456123', 'B', 'path/to/license4.png', '1995-05-25', NOW(), NULL),
-    (gen_random_uuid(), 'Paulo Fernandes', '65748392000101', '321654987', 'A', 'path/to/license5.png', '1988-09-09', NOW(), NULL);
+    ('a2284da1-e871-4e57-8af1-fce994aa1824', 'Elias Lima', '12345678000195', '28517540912', 'A', 'path/to/license1.png', '1985-03-15', NOW(), NULL);
+
+INSERT INTO "Order" ("Id", "DeliveryStatus", "CreatedAt", "UpdatedAt") VALUES
+    (gen_random_uuid(), 'Pending', NOW(), NULL),
+    ('30eda151-20f9-4e85-b373-063b35a178fe', 'In Progress', NOW(), NULL),
+    ('219024a1-1e01-4b9f-b262-aae5deae3cff', 'Completed', NOW(), NOW()),
+    (gen_random_uuid(), 'Pending', NOW(), NULL),
+    (gen_random_uuid(), 'Pending', NOW(), NULL),
+    (gen_random_uuid(), 'Pending', NOW(), NOW()),
+    (gen_random_uuid(), 'Pending', NOW(), NULL),
+    (gen_random_uuid(), 'Pending', NOW(), NULL),
+    (gen_random_uuid(), 'Pending', NOW(), NOW()),
+    (gen_random_uuid(), 'Pending', NOW(), NULL);
+
+INSERT INTO "DeliveryAssignment" ("Id", "CourierId", "OrderId", "AssignedAt", "CompletedAt", "CreatedAt", "UpdatedAt") VALUES
+    (gen_random_uuid(), 'a2284da1-e871-4e57-8af1-fce994aa1824', '30eda151-20f9-4e85-b373-063b35a178fe', NOW(), NULL, NOW(), NULL),
+    (gen_random_uuid(), 'a2284da1-e871-4e57-8af1-fce994aa1824', '219024a1-1e01-4b9f-b262-aae5deae3cff', NOW(), NOW(), NOW(), NOW());
